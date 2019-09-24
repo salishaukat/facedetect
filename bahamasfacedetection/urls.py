2b0946656009a82ddd0b5aed42d5462c0189a7e6
Abdullah Dagia <abdullahhanif1993@hotmail.com>
2019-09-25 03:20:42 +0500

    admin update

--- bahamasfacedetection/urls.py
+++ bahamasfacedetection/urls.py
@@ -18,7 +18,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+admin.site.site_header = "Find me Bahamas"
+admin.site.site_title = "Find me Bahamas Admin Portal"
+admin.site.index_title = "Welcome to Find me Bahamas Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
